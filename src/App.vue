f6d12b8cdaff6b1355dca835dcc3526c1124b34a
jgzuara <jgilzuara@gmail.com>
2022-08-08 20:53:28 +0200

    Remove useless files

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,11 @@
-
-
 <template>
   <header class="row">
     <RouterLink to="/" class="text-center">
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="425" height="125" />
     </RouterLink>
     <div class="wrapper">     
       <h1 class="text-center">Code challenge - TV Shows</h1>
-      <!--<nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>-->
     </div>
   </header>
   <RouterView />
-  <!--<home-view />-->
 </template>
-
-<script setup>
-  //import { RouterLink, RouterView } from 'vue-router'
-  //import HelloWorld from './components/HelloWorld.vue'
-  import HomeView from './views/HomeView.vue'
-</script>
-
-<style scoped>
-/*.bg-fashionette {
-  background-color: #e5c1c4;
-  font-family: AvenirNext,sans-serif;
-}
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-*/
-</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,22 @@
 <template>
-  <!--<main>-->
-    <div class="row">
-      <div class="col-12 tex-center">
-        <!--
-        <RouterLink to="/">
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-door" viewBox="0 0 16 16">
-            <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z"/>
-          </svg>      
-        </RouterLink>
-        -->
-        <search-text
-          :parameters="params"
-          @makeSearch="makeSearch"
-        />
-        <list-films
-          :films="filmsData"
-        />
-      </div>      
-    </div>   
-  <!--</main>-->
+  <div class="row">
+    <div class="col-12 tex-center">
+      <search-text
+        :parameters="params"
+        @makeSearch="makeSearch"
+      />
+      <list-films
+        :films="filmsData"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
   import useTvmaze from '@/controllers/useTvmaze'
   import SearchText from '@/components/SearchText.vue'
   import ListFilms from '@/components/ListFilms.vue'
-  import { reactive, watch, ref } from 'vue'
+  import { reactive, ref } from 'vue'
 
   export default {
     name: 'HomeView',
@@ -33,7 +24,7 @@
       SearchText,
       ListFilms
     },
-    setup(props, { emit }){
+    setup(){
       const { listFilms } = useTvmaze()
       const filmsData = ref(null)
       const response = ref(null)
@@ -43,14 +34,7 @@
         page: 0
       })
 
-      // Watch
-      /*
-      watch(params, (val, prevVal) => {
-        listFilms(params)
-      })
-      */
       const makeSearch = async () => {
-
         if (params.searchText != localStorage.getItem("lastSearch")) {
           response.value = await listFilms(params)
           filmsData.value = response.value.rows
@@ -60,11 +44,6 @@
         } else {
           filmsData.value = JSON.parse(localStorage.getItem("lastFilms"))
         }
-
-
-
-        //console.log('EL MAKE SEARCH')
-        //console.log(filmsData.value)
       }
 
       return {
